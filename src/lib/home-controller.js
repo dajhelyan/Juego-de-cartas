97989dee3ef4ba414650b7262f884b79d7c37212
dajhelyan <assiaxramx10@gmail.com>
2019-09-29 20:59:04 -0500

    realizando funcion que recibe un array de 12 opciones, las mezcla aleatoriamente y corta 4 cartas unicas, hace una copia para obtener los pares y finalmente chocolatea la baraja con las 8 cartas para mostar... Fixed #7

--- src/lib/home-controller.js
+++ src/lib/home-controller.js
@@ -1,10 +1,12 @@
 export const changeHash = (hash) => { window.location.hash = hash };
 
+const arrCartas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
 // retorna un numero aleatorio entre inicio y fin
 const numeroAleatorio = (inicio, fin) => {
     return Math.floor(Math.random() * (fin - inicio + 1)) + inicio;
 }
-  
+  // posiciones 
 const chocolateate = (lista) => {
     // maximo indice de la lista
     const maxIndex = lista.length - 1
@@ -20,3 +22,80 @@ const chocolateate = (lista) => {
     }
     return nuevaLista;
 }
+
+const cartasParaMostrar = (arr) => {
+    const newCartas = chocolateate(arr);
+    let fourCartas = newCartas.slice(0,4);
+    const baraja = [...fourCartas, ...fourCartas];
+    const cartasALaMesa = chocolateate(baraja);
+    return cartasALaMesa;
+}
+
+console.log(cartasParaMostrar(arrCartas))
+
+const printImg = () => {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const obteniendo4de12 = (arr) => {
+    // chocolatear arrCartas
+    const newCartas = chocolateate(arr);
+    return newCartas.slice(0,4);
+
+    //despues de chocolatear retornar 4 primeras cartas
+}
+ */
+/* const agregandoPares = (cartas) => {
+    const baraja = [...cartas, ...cartas]
+    return baraja;
+} */
+
+/* const cartasALaMesa = chocolateate(agregandoPares(obteniendo4de12(arrCartas, 'ooo')))
+console.log(cartasALaMesa, 'jsjj')
+
+console.log(agregandoPares(obteniendo4de12(arrCartas, 'ooo')));
+
+console.log(obteniendo4de12(arrCartas, 'ooo')) */
+
+/* for (let i = 0; i < cartasALaMesa.length; i++) {
+    const element = cartasALaMesa[i];
+    console.log(`animales (${element}).jpg`)
+} */
